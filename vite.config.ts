109e02bcae6e9e18b8be8a1001b5f2c286126ac2
里丰 <1418685259@qq.com>
2025-07-04 09:09:32 +0000

    fit github page path

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// 根据环境变量动态设置 base 路径
+const isGithubPages = process.env.GITHUB_PAGES === '1';
 export default defineConfig({
+  base: isGithubPages ? '/easytools/' : '/',
   plugins: [react()],
 })
